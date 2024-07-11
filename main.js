32cf6b6471338894926eb0b31b69c6ff4876dff8
SnGermen <144361057+SnGermen@users.noreply.github.com>
2024-07-11 17:57:18 +0200

    m

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-let buttons = document.querySelectorAll(".about__block")
-
+const buttons = document.querySelectorAll(".about__block")
+const items = document.querySelectorAll(".about__midle")
 buttons.forEach((i) => {
   i.addEventListener("click", () => {
+    let atributs = i.getAttribute("data-tab")
+    let getAtribut = document.querySelector(atributs)
+
 
     buttons.forEach((deleted) => deleted.classList.remove("about__block_active"))
-
+    items.forEach((deleted) => deleted.classList.remove("about__midle_active"))
     i.classList.add("about__block_active")
+    getAtribut.classList.add("about__midle_active")
   })
 })
 //Tabs
